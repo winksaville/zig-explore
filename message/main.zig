eb985165793aa88e1c6728510b2f609e3307d36c
Wink Saville <wink@saville.com>
2018-07-27 12:29:53 -0700

    Add bodyInit and bodyFormat as methods to BodyType types for Message
    These are now required methods of structures "extending" Messages

--- message/main.zig
+++ message/main.zig
@@ -3,44 +3,67 @@ const warn = std.debug.warn;
 const mem = std.mem;
 const math = std.math;
 
-fn Message(comptime T: type) type {
+fn Message(comptime BodyType: type) type {
     return struct {
         const Self = this;
 
         cmd: u64,
-        body: T,
+        body: BodyType,
 
         fn init(cmd: u64) Self {
-        //fn init(cmd: u64, bodyInit: fn(t: *T) void) Self {
             var self: Self = undefined;
             self.cmd = cmd;
-            //bodyInit(&self.body);
+            BodyType.bodyInit(&self);
             return self;
+        }
+
+        pub fn format(self: *const Self,
+            comptime fmt: []const u8,
+            context: var,
+            comptime FmtError: type,
+            output: fn (@typeOf(context), []const u8) FmtError!void
+        ) FmtError!void {
+            if (mem.eql(u8, fmt[0..], "p")) { return std.fmt.formatAddress(self, fmt, context, FmtError, output); }
+            else {
+                try std.fmt.format(context, FmtError, output, "{{");
+                try std.fmt.format(context, FmtError, output, "cmd={},", self.cmd);
+                try BodyType.bodyFormat(self, fmt, context, FmtError, output);
+                try std.fmt.format(context, FmtError, output, "}}");
+            }
         }
     };
 }
-const MyMessage = Message(struct {data: [3]u8});
 
-fn MyMessageBodyInit(m: *MyMessage) void {
-    mem.set(u8, m.body.data[0..m.body.data.len], 'Z');
-}
+const MyMessage = Message(struct {
+    data: [3]u8,
+
+    fn bodyInit(m: *MyMessage) void {
+        mem.set(u8, m.body.data[0..m.body.data.len], 'Z');
+    }
+
+    pub fn bodyFormat(m: *const MyMessage,
+        comptime fmt: []const u8,
+        context: var,
+        comptime FmtError: type,
+        output: fn (@typeOf(context), []const u8) FmtError!void
+    ) FmtError!void {
+        try std.fmt.format(context, FmtError, output, "data={{");
+        for (m.body.data) |v| {
+            try std.fmt.format(context, FmtError, output, "{x},", v);
+        }
+        try std.fmt.format(context, FmtError, output, "}},");
+    }
+});
 
 pub fn main() void {
-    //var msg = MyMessage.init(123, MyMessageBodyInit);
     var msg = MyMessage.init(123);
-    MyMessageBodyInit(&msg);
-    warn("cmd={} data.len={} data={}\n", msg.cmd, msg.body.data.len, msg.body.data[0..]);
+    warn("msg={p}\n", &msg);
+    warn("msg={}\n", &msg);
+    //warn("cmd={} data.len={} data={}\n", msg.cmd, msg.body.data.len, msg.body.data[0..]);
 
-    warn("msg.cmd={}\n", msg.cmd);
     const some_data = "a";
-    //var copy_len = math.min(msg.body.data.len, some_data.len);
-    //mem.copy(u8, msg.body.data[0..], some_data[0..copy_len]);
-    for (some_data) |v,i| {
-        if (i >= msg.body.data.len) {
-            //warn("overflowed msg.body.data at index {}\n", i);
-            break;
-        }
-        msg.body.data[i] = v;
-    }
-    warn("msg.body.data.len={} msg.body.data={}\n", msg.body.data.len, msg.body.data[0..]);
+    var copy_len = math.min(msg.body.data.len, some_data.len);
+    mem.copy(u8, msg.body.data[0..], some_data[0..copy_len]);
+    warn("msg={}\n", &msg);
+    //warn("msg.body.data.len={} msg.body.data={}\n", msg.body.data.len, msg.body.data[0..]);
 }
