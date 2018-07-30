6cb484e15b43a4ea8e32f2d5f480c432e17480a6
Wink Saville <wink@saville.com>
2018-07-30 11:23:35 -0700

    wip: Figure out ptr casting

---
+++ ptr-casts/README.md
@@ -0,0 +1,14 @@
+# Zig ptr casts
+
+Figure out how to so pointer casting in "init" and "getXxxAs" 
+
+## Building/testing
+```bash
+$ zig test ptr_casts.zig
+```
+
+## Clean
+Remove `zig-cache/` directory
+```bash
+$ rm -rf ./zig-cache/
+```
---
+++ ptr-casts/ptr_casts.zig
@@ -0,0 +1,43 @@
+const std = @import("std");
+const assert = std.debug.assert;
+const mem = std.mem;
+
+const Message = struct {
+    const Self = this;
+    const BodyPtr = *@OpaqueType();
+
+    pub body_ptr: BodyPtr,
+
+    pub fn init(body_ptr: var) Self {
+        var self = Self {
+            .body_ptr = @ptrCast(Message.BodyPtr, body_ptr),
+        };
+        return self;
+    }
+
+    pub fn getBodyPtrAs(self: *const Self, comptime T: type) T {
+        return @ptrCast(T, self.body_ptr);
+    }
+};
+
+const MyMsgBody = struct {
+    const Self = this;
+    pub data: [3]u8,
+
+    pub fn init() Self {
+        var self: Self = undefined;
+        mem.set(u8, self.data[0..], 'Z');
+        return self;
+    }
+};
+
+test "Message" {
+    var myMsgBody = MyMsgBody.init();
+    assert(mem.eql(u8, myMsgBody.data[0..], "ZZZ"));
+
+    var myMsg1 = Message.init(&myMsgBody);
+    assert(@ptrToInt(myMsg1.body_ptr) == @ptrToInt(&myMsgBody));
+
+    var myMsgBody2 = myMsg1.getBodyPtrAs(*MyMsgBody);
+    assert(mem.eql(u8, myMsgBody2.data[0..], "ZZZ"));
+}
