62b474e5fd796d732dae02aaa341052a6dd90959
Wink Saville <wink@saville.com>
2018-09-17 22:12:16 -0700

    Reworked test.

--- u0/u0.zig
+++ u0/u0.zig
@@ -5,70 +5,123 @@ const builtin = @import("builtin");
 const TypeId = builtin.TypeId;
 
 test "u0" {
-    assert(@typeId(u0) == TypeId.Int);
-    assert(@typeId(u0) != TypeId.Void);
+    //warn("\n");
 
-    var zero: u0 = 0;
-    assert(zero == 0);
-    assert(@sizeOf(@typeOf(zero)) == 0);
-    assert(@sizeOf(u0) == 0);
+    var usize1: usize = undefined;
+    var usize2: usize = undefined;
 
-    var z: u0 = 0;
-    zero = z;
-    assert(zero == z);
-    assert(&zero == null);
-    //warn("zero={}\n", zero); // Can't print yet bug #557
-    //var z1 = @intCast(u1, zero); // Causes compiler seg fault
-
-    var pZero: *u0 = &zero;
-    //warn("pZero={}\n", pZero); // Can't print yet bug #557
-
-    assert(pZero == null);
-    //assert(@ptrCast(?*u0, pZero) == null); // Crashes compiler with seg fault
-
+    // Test with u1
     var one: u1 = 1;
+    assert(one == 1);
+    assert(@sizeOf(@typeOf(one)) == 1);
+    assert(@sizeOf(u1) == 1);
+    assert(@intCast(u8, one) == 1);
+
+    var pOne: *u1 = &one;
+    var pOne2: *u1 = &one;
+    assert(pOne.* == pOne2.*);
+    assert(pOne == &one);
+    assert(pOne2 == &one);
+    assert(pOne == pOne2);
+    assert(&pOne != &pOne2);
+
+    usize1 = @ptrToInt(pOne);
+    usize2 = @ptrToInt(pOne2);
+    assert(usize1 == usize2);
+    usize1 = @ptrToInt(&pOne);
+    usize2 = @ptrToInt(&pOne2);
+    assert(usize1 != usize2);
+
     var pOneOptional: ?*u1 = &one;
-    warn("pOneOptional={*}\n", pOneOptional);
+    var pOneOptional2: ?*u1 = &one;
+    assert(pOneOptional == pOneOptional2);
+    assert(&pOneOptional != &pOneOptional2);
+    assert(pOneOptional != null);
+    assert(pOneOptional == &one);
     if (pOneOptional != null) {
         assert(pOneOptional.?.* == 1);
     } else {
         unreachable; // assert(pOneOptional == null);
     }
 
+    // "Same" tests with u0:
+
+    // These are expected results
+    var zero: u0 = 0;
+    assert(zero == 0);
+    assert(@sizeOf(@typeOf(zero)) == 0);
+    assert(@sizeOf(u0) == 0);
+    assert(@intCast(u8, zero) == 0);
+
+    // These are expected results expect the last one assert(&pZero == &pZero2)
+    var pZero = &zero;
+    var pZero2 = &zero;
+    assert(pZero.* == pZero2.*);
+    assert(pZero == &zero);
+    assert(pZero2 == &zero);
+    assert(pZero == pZero2);
+    assert(&pZero == &pZero2); // This seems odd??
+
+    // Below when trying to convert pZero to int it says:
+    //   "pointer to size 0 type has no address"
+    // These is unexpected especially above I can "take an address"
+    //usize1 = @ptrToInt(pZero); // compile error: pointer to size 0 type has no address
+    //usize2 = @ptrToInt(pZero2); // compile error: pointer to size 0 type has no address
+    //assert(usize1 == usize2);
+    //usize1 = @ptrToInt(&pZero); // compile error: pointer to size 0 type has no address
+    //usize2 = @ptrToInt(&pZero2); // compile error: pointer to size 0 type has no address
+    //assert(usize1 != usize2);
+
+    // These are expected results
     var pZeroOptional: ?*u0 = &zero;
-    //warn("pZeroOptional={*}\n", pZeroOptional); // LLVM ERROR: Borken module found, compilation aborted!
-    assert(&zero == null);         // This is true
-    assert(pZeroOptional != null); // And this is true
-    assert(pZeroOptional == &zero);// And this is true
+    var pZeroOptional2: ?*u0 = &zero;
+    assert(pZeroOptional == pZeroOptional2);
+    assert(&pZeroOptional != &pZeroOptional2);
+    assert(pZeroOptional != null);
+    assert(pZeroOptional == &zero);
     if (pZeroOptional != null) {
         assert(pZeroOptional.?.* == 0);
     } else {
-        unreachable; // Currently unreachable
-        //assert(pZeroOptional == null);
+        unreachable; //assert(pZeroOptional == null);
     }
 
+    // Using "packed" cause compiler error
+    // zig: ../src/analyze.cpp:499: ZigType* get_pointer_to_type_extra(CodeGen*, ZigType*, bool, bool, PtrLen, uint32_t, uint32_t, uint32_t): Assertion `byte_alignment == 0' failed.
+    // Aborted (core dumped)
+    //const Su0 = packed struct {
     const Su0 = struct {
-        z: u0,
+        f0: u8,
+        z1: u0,
+        z2: u0,
     };
-    var su0 = Su0 { .z = 0, };
-    assert(@sizeOf(Su0) == 0);
-    assert(&su0 == null);
-    assert(su0.z == 0);
 
-    // Empty structs are similar to u0
-    const Empty = struct {};
-    var empty: Empty = undefined;
-    assert(@sizeOf(Empty) == 0);
-    assert(&empty == null);
+    // These are expected
+    var su0 = Su0 { .f0 = 0, .z1 = 0, .z2 = 0 };
+    assert(@sizeOf(Su0) == 1);
+    assert(su0.z1 == 0);
+    assert(su0.z2 == 0);
+    assert(su0.z1 == su0.z2);
+    assert(&su0.z1 == &su0.z2);
+    var pZ1 = &su0.z1;
+    var pZ2 = &su0.z2;
+    assert(pZ1 == pZ2);
 
-    var v: void = {};
-    assert(v == {});
-    assert(&v == null);
-    assert(@sizeOf(@typeOf(v)) == 0);
-    var pV = &v;
-    assert(pV == &v);
-    assert(pV == null);
-    assert(pV.* == {});
+    var pSu0 = &su0;
+    var pSu0_u8 = @ptrCast(*align(1) u8, pSu0);
+    var pSu0_f0_u8 = @ptrCast(*align(1) u8, &pSu0.f0);
+    assert(pSu0_u8 == pSu0_f0_u8);
+    var f0_offset: usize = @offsetOf(Su0, "f0");
+    assert(f0_offset == 0);
+
+    // If I can take an address of su0.z1 and su0.z1 and assert(pZ1 == pZ2)
+    // then I should be able to get its offset and su0.z2 offset and assert(z1_offset == z2_offset).
+    //var z1_offset: usize @offsetOf(Su0, "z1"); // compile error: zero-bit field 'z1' in struct 'Su0' has no offset
+    //var z2_offset: usize @offsetOf(Su0, "z2"); // compile error: zero-bit field 'z1' in struct 'Su0' has no offset
+    //assert(z1_offset == z2_offset); // My expectation?
+
+    //usize1 = @ptrToInt(pZ1); // compile error: pointer to size 0 type has no address
+    //usize2 = @ptrToInt(pZ2); // compile error: pointer to size 0 type has no address
+    //assert(usize1 == usize2);
 }
 
 fn x() u0 {
