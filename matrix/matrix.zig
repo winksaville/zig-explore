8e161c6b3c7da241a788faf91c48ddcfbb54f1b8
Wink Saville <wink@saville.com>
2018-10-29 13:28:49 -0700

    Explore creating a matrix

---
+++ matrix/matrix.zig
@@ -0,0 +1,150 @@
+const builtin = @import("builtin");
+const TypeInfo = builtin.TypeInfo;
+const TypeId = builtin.TypeId;
+
+const std = @import("std");
+const math = std.math;
+//const meta = std.meta;
+const assert = std.debug.assert;
+const warn = std.debug.warn;
+
+pub fn Matrix(comptime T: type, comptime m: usize, comptime n: usize) type {
+    return struct.{
+        const Self = @This();
+        const row_cnt = m;
+        const col_cnt = n;
+
+        pub data: [m][n]T,
+
+        /// Initialize Matrix to a value
+        pub fn init() Self {
+            return Self.{ .data = undefined };
+        }
+
+        /// Return true of pSelf.data == pOther.data
+        pub fn eql(pSelf: *const Self, pOther: *const Self) bool {
+            for (pSelf.data) |row, i| {
+                for (row) |val, j| {
+                    if (val != pOther.data[i][j]) return false;
+                }
+            }
+            return true;
+        }
+
+        /// Print the Matrix
+        pub fn print(pSelf: *const Self, s: []const u8) void {
+            warn("{}", s);
+            for (pSelf.data) |row, i| {
+                warn("{}: []{}.{{ ", i, @typeName(T));
+                for (row) |val, j| {
+                    warn("{.7}{} ", val, if (j < (row.len - 1)) "," else "");
+                }
+                warn("}},\n");
+            }
+        }
+    };
+}
+
+fn getDef(comptime defs: []TypeInfo.Definition, name: []const u8) !TypeInfo.Definition {
+    for (defs) |def| {
+        if (std.mem.eql(u8, def.name, name)) {
+            return def;
+        }
+    }
+    return error.NoDef;
+}
+
+fn getMemberIdx(comptime T: type, name: []const u8) !usize {
+    var i: usize = 0;
+    while (i < @memberCount(T)) : (i += 1) {
+        if (std.mem.eql(u8, @memberName(T, i), name)) {
+            return i;
+        }
+    }
+    return error.NoMember;
+}
+
+/// Multiply Matrixes m1 by m2
+pub fn MatrixMuliplier(comptime m1: type, comptime m2: type) type {
+    const m1_def = try getDef(@typeInfo(m1).Struct.defs, "Self");
+    const m2_def = try getDef(@typeInfo(m2).Struct.defs, "Self");
+
+    const m1_row_cnt = m1_def.data.Type.row_cnt;
+    const m1_col_cnt = m1_def.data.Type.col_cnt;
+    const m1_data_idx = try getMemberIdx(m1, "data");
+    const m1_DataType = @typeInfo(@typeInfo(@memberType(m1, m1_data_idx)).Array.child).Array.child;
+
+    const m2_row_cnt = m2_def.data.Type.row_cnt;
+    const m2_col_cnt = m2_def.data.Type.col_cnt;
+    const m2_data_idx = try getMemberIdx(m2, "data");
+    const m2_DataType = @typeInfo(@typeInfo(@memberType(m1, m2_data_idx)).Array.child).Array.child;
+
+    // What other validations should I check
+    if (m1_DataType != m2_DataType) {
+        return @compileError("Matrix m1.data type != m2.data type");
+    }
+    if (m1_col_cnt != m2_row_cnt) {
+        return @compileError("Matrix m1.col_cnt != m2.row_cnt");
+    }
+    const DataType = m1_DataType;
+    const row_cnt = m1_row_cnt;
+    const col_cnt = m2_col_cnt;
+    return struct.{
+        pub fn mul(mt1: *const m1, mt2: *const m2) Matrix(DataType, row_cnt, col_cnt) {
+            var r = Matrix(DataType, row_cnt, col_cnt).init();
+            comptime var i: usize = 0;
+            inline while (i < row_cnt) : (i += 1) {
+                //warn("mul {}:\n", i);
+                comptime var j: usize = 0;
+                inline while (j < col_cnt) : (j += 1) {
+                    //warn(" ({}:", j);
+                    comptime var k: usize = 0;
+                    inline while (k < col_cnt) : (k += 1) {
+                        var val = mt1.data[i][k] * mt2.data[k][j];
+                        if (k == 0) {
+                            r.data[i][j] = val;
+                            //warn(" {}:{}={} * {}", k, val, mt1.data[i][k], mt2.data[k][j]);
+                        } else {
+                            r.data[i][j] += mt1.data[i][k] * mt2.data[k][j];
+                            //warn(" {}:{}={} * {}", k, val, mt1.data[i][k], mt2.data[k][j]);
+                        }
+                    }
+                    //warn(" {})\n", r.data[i][j]);
+                }
+            }
+            return r;
+        }
+    };
+}
+
+test "abc" {
+    warn("\n");
+    var m1 = Matrix(f32, 2, 2).init();
+    m1.data = [][2]f32.{
+        []f32.{ 1, 2 },
+        []f32.{ 3, 4 },
+    };
+    var m2 = Matrix(f32, 2, 2).init();
+    m2.data = [][2]f32.{
+        []f32.{ 5, 6 },
+        []f32.{ 7, 8 },
+    };
+    m1.print("matrix.mul m1:\n");
+    m2.print("matrix.mul m2:\n");
+    const m3 = MatrixMuliplier(@typeOf(m1), @typeOf(m2)).mul(&m1, &m2);
+    m3.print("matrix.mul m3:\n");
+
+    var expected = Matrix(f32, 2, 2).init();
+    expected.data = [][2]f32.{
+        []f32.{
+            (m1.data[0][0] * m2.data[0][0]) + (m1.data[0][1] * m2.data[1][0]),
+            (m1.data[0][0] * m2.data[0][1]) + (m1.data[0][1] * m2.data[1][1]),
+        },
+        []f32.{
+            (m1.data[1][0] * m2.data[0][0]) + (m1.data[1][1] * m2.data[1][0]),
+            (m1.data[1][0] * m2.data[0][1]) + (m1.data[1][1] * m2.data[1][1]),
+        },
+    };
+    expected.print("matrix.mul expected:\n");
+    assert(m3.eql(&expected));
+}
